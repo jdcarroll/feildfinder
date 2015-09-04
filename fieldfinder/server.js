920276e24c604a7841c24fe62285cf1ef745eba9
Kelly Rhodes <krhodes1@fullsail.edu>
2015-09-04 12:17:51 -0400

    links to css, images updated

--- fieldfinder/server.js
+++ fieldfinder/server.js
@@ -5,8 +5,7 @@ var app = express();
 app.set('views',__dirname);
 app.set('view engine','ejs');
 
-
-app.use(express.static(__dirname + '/public'));
+app.use("/public", express.static(path.join(__dirname, 'public')));
 
 app.get('/',function(req,res){
 
@@ -24,3 +23,7 @@ app.get('/results',function(req,res){
 
 app.listen(7000);
 console.log('Listening on port 7000');
+
+
+
+
