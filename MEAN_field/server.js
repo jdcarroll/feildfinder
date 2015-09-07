2dcd29b13afcbffb0fb6d8540aaef5fbc45ba909
Kelly Rhodes <krhodes1@fullsail.edu>
2015-09-07 14:30:05 -0400

    connect to db

--- MEAN_field/server.js
+++ MEAN_field/server.js
@@ -4,28 +4,17 @@
 var express        	= require('express');
 var path 			= require('path');
 var app            	= express();
-var mongoose       	= require('mongoose');
 var bodyParser     	= require('body-parser');
 var methodOverride 	= require('method-override');
 var mongojs 		= require('mongojs');
 
-
-
 // configuration ===========================================
 	
-// config files
-var db = require('./config/db');
-
-// below is from Jeff's app; above is from the MEAN app
-// see the mongoose connect below as well, we can set up a db server for the project
-var db 	= mongojs('fieldlist', ['fieldlist']);
+var db = mongojs('test', ['fields']);
+console.log(db);
 
 // set our port
 var port = process.env.PORT || 7000; 
-
-// connect to our mongoDB database 
-// (commented out after you enter in your own credentials)
-// mongoose.connect(db.url); 
 
 
 // get all data/stuff of the body (POST) parameters
