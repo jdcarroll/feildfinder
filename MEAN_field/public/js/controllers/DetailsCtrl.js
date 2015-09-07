18738e88aa45481d1fbfc5a32d5079c936ca9672
Kelly Rhodes <krhodes1@fullsail.edu>
2015-09-07 11:57:23 -0400

    routes tweak

--- MEAN_field/public/js/controllers/DetailsCtrl.js
+++ MEAN_field/public/js/controllers/DetailsCtrl.js
@@ -5,4 +5,15 @@ angular.module('DetailsCtrl', []).controller('DetailsController', function($scop
 
 	// Place weather API stuff here
 
+	// Need to grab the field id from the url
+	// save the field id as a var
+	// pass the field id to the database to pull the results list (length = 1)
+	// save results to $scope.results
+
+	// to be deleted later
+	// testing without database
+		$scope.results = [
+		  {name: 'Field 1', address: '123 Main Street', city: 'Orlando', state: 'FL', zip: '32804', phone: '(407) 123-4567'},
+		];
+
 });
